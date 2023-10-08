e1f52f0e96f685b2d6e37d824984259d05b33725
fiifibrew <fiifibrew12@gmail.com>
2023-10-08 16:51:24 -0700

    Corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * The main function is the entry point of the program.
+ * It prints the size of various types.
+ *
+ * @return 0 on success
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * The main function is the entry point of the program.
+ * It prints the size of various types.
+ *
+ * @return 0 on success
+ */
 	int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
