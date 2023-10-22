732ffa17db55bae4641097d04699ea6916e1d590
fiifibrew <fiifibrew12@gmail.com>
2023-10-22 09:12:40 -0700

    Corrections on 4

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,5 +10,6 @@
 
 int add(int one, int two)
 {
+	int num1, num2;
 	return (num1 + num2);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * &a - character to be checked
- * Return: 1 for lower case character or o for anything else
- */
+* _isalpha - checks for alphabetic character
+*
+* @c: parameter for function
+*
+*
+* Return: 1 if c is alphabet and 0 if not
+*/
 
-int _islower(int a)
+
+int _isalpha(int c)
 {
-        if (a >= 97 && a <= 122)
-        {
-        return (1);
-        }
-        return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+
 }
